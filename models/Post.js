ea1f5014aae4d44d93897662bc1ba033cc7b3252
Kamy Moussavi <32441857+ogKamy@users.noreply.github.com>
2019-06-01 16:07:59 -0400

    added authentication and profile routes

--- models/Post.js
+++ models/Post.js
@@ -1,26 +1,55 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const UserSchema = new mongoose.Schema({
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  text: {
+    type: String,
+    required: true
+  },
   name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
+    type: String
   },
   avatar: {
     type: String
   },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
   date: {
     type: Date,
     default: Date.now
   }
 });
 
-module.exports = User = mongoose.model("user", UserSchema);
+module.exports = Post = mongoose.model("post", PostSchema);
