9ee2a0fd0251bb3deb22b2679212d88817e44a45
Kamy Moussavi <32441857+ogKamy@users.noreply.github.com>
2019-06-09 10:01:20 -0400

    Finished dashboard

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,11 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from "../actions/types";
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE,
+  GET_PROFILES,
+  GET_REPOS
+} from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -8,14 +15,21 @@ const initialState = {
   error: {}
 };
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
     case GET_PROFILE:
+    case UPDATE_PROFILE:
       return {
         ...state,
         profile: payload,
+        loading: false
+      };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
         loading: false
       };
     case PROFILE_ERROR:
@@ -25,7 +39,18 @@ export default function(state = initialState, action) {
         loading: false
       };
     case CLEAR_PROFILE:
-      return { ...state, profile: null, repos: [], loading: false };
+      return {
+        ...state,
+        profile: null,
+        repos: [],
+        loading: false
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
+        loading: false
+      };
     default:
       return state;
   }
